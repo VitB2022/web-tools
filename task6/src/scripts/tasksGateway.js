83e0e04e52efb0116ec9cbc58ed83e23c2858cb7
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-26 12:11:18 +0300

    added settings

---
+++ task6/src/scripts/tasksGateway.js
@@ -0,0 +1,30 @@
+const baseUrl = 'https://62d57413d4406e52355b320b.mockapi.io/api/v1/todo-list';
+
+export const getTasksList = () => {
+  return fetch(baseUrl).then(response => response.json());
+};
+
+export const createTask = taskData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(taskData),
+  });
+};
+
+export const updateTask = (taskId, updatedTaskData) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(updatedTaskData),
+  });
+};
+export const deleteTask = taskId => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  });
+};
