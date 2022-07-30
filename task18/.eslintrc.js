9cb9403c131141c0eed132c91a5bf03d526d3298
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-30 20:22:57 +0300

    added settings

---
+++ task18/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "eslint-config-airbnb-base",
+  rules: { "no-console": 2 },
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+  },
+};
