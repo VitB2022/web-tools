c2609b18ce05ecd9ed9f244053fce3437a60def8
Vitaliy Bednenko <cupony2018@gmail.com>
2022-07-30 12:13:33 +0300

    corrected

--- task16/webpack.config.js
+++ task16/webpack.config.js
@@ -2,46 +2,59 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /.s?css$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
-              outputPath: "images",
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+  const config = {
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
     },
-    compress: true,
-    port: 9000,
-  },
+    module: {
+      rules: [
+        {
+          test: /.s?css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 8192,
+                outputPath: "images",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+    },
+  };
+  if (isProduction) {
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      })
+    );
+  }
+
+  return config;
 };
